59c4867d820499f79d71188f62c2f5a3491d9c9d
Krzysztof Grabowski <krzysztof.grabowski@cronn.de>
2023-06-04 18:31:55 +0200

    Add ssd1306 Oled

--- main/PlantMonitoringSensorAio.cpp
+++ main/PlantMonitoringSensorAio.cpp
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
+#include "string.h"
 #include "esp_system.h"
 #include "esp_log.h"
 #include "Adafruit_Stemma_soil_sensor.h"
+#include "ssd1306.h"
 
 #define I2C_MASTER_NUM 0
 #define I2C_SDA_PIN 21
 #define I2C_SCL_PIN 22
-#define DELAY_TIME_BETWEEN_READINGS_MS 100
+#define SSD1306_DISPLAY_ADDRESS 0x3C
+#define SSD1306_DISPLAY_WIDTH 128
+#define SSD1306_DISPLAY_HEIGHT 64
+#define DELAY_TIME_BETWEEN_READINGS_MS 1000
 
 static const char *TAG = "Plant Monitoring Sensor AIO";
 
@@ -21,6 +22,10 @@ extern "C" void app_main(void)
 
     esp_log_level_set("*", ESP_LOG_INFO);
     ESP_ERROR_CHECK(adafruit_stemma_soil_sensor_init(I2C_MASTER_NUM, I2C_SDA_PIN, I2C_SCL_PIN));
+
+    SSD1306_t dev;
+    dev._address = SSD1306_DISPLAY_ADDRESS;
+    ssd1306_init(&dev, SSD1306_DISPLAY_WIDTH, SSD1306_DISPLAY_HEIGHT);
 
     while (1)
     {
@@ -35,10 +40,17 @@ extern "C" void app_main(void)
 
         if (ret == ESP_OK)
         {
-            ESP_LOGI(TAG, "Adafruit Stemma sensor value: =%f", temperature_value);
+            ESP_LOGI(TAG, "Adafruit Stemma sensor value: =%.2f", temperature_value);
         }
-        
-    
+
+        ssd1306_clear_screen(&dev, false);
+
+        char buf[50];
+        sprintf(buf, "Moisture: %u", moisture_value);
+        ssd1306_display_text(&dev, 0, buf, strlen(buf), false);
+        sprintf(buf, "Temp: %.2f", temperature_value);
+        ssd1306_display_text(&dev, 1, buf, strlen(buf), false);
+
         vTaskDelay((DELAY_TIME_BETWEEN_READINGS_MS) / portTICK_PERIOD_MS);
     }
 }
